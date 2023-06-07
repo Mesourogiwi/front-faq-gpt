2502aab22e0943dd3c5715ec85e0f6bd586d646d
Maira Berlato <mairaberlatof@gmail.com>
2023-06-07 18:13:02 -0300

    widgets main page

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Users } from './Users';
 import { SessionMessages } from './SessionMessages';
+import { Widgets } from './Widgets';
 
 export const Api: React.FC = () => {
   return (
     <>
       <Users />
+      <Widgets />
       <SessionMessages />
     </>
   );
